cc9d293659d14d90ab63d9f8c02c7c652a6a8c9d
Vruti Gadhiya <vrutigadhiya07032006@gmail.com>
2025-12-10 23:28:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const project1 = document.getElementById("project1");
+const project2 = document.getElementById("project2");
+
+project1.addEventListener("click", () => {
+    // Open video in a new browser tab
+    window.open("./ASSETE/R&D Proposl.mp4", "_blank");
+});
+
+project2.addEventListener("click", () => {
+    window.open("https://github.com/vrutigadhiya", "_blank");
+});
